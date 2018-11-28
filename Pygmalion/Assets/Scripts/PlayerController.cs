08b0dc1f6ddbc9a5a7b2ac4e4f8131efed8ef0e7
Hayen03 <jetzer.leo@gmail.com>
2018-11-27 22:03:48 -0500

    Creation de la scene de test et mouvement horizontal du joueur

---
+++ Pygmalion/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	// Ref vers d'autre composant
+	public Animator anim;
+	public Rigidbody2D body;
+
+	// Var pour les deplacements
+	public float horizontal_speed = 5.0f;
+
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animator>();
+		body = GetComponent<Rigidbody2D>();
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		// Base
+		float haxis = Input.GetAxisRaw("Horizontal");
+		Vector3 scale = transform.localScale;
+		float scalex = Mathf.Abs(scale.x);
+		body.velocity = new Vector2(horizontal_speed * haxis, body.velocity.y);
+		// flip the sprite if necessary
+		if (haxis < 0){
+			scalex = -scalex;
+		}
+		transform.localScale = new Vector3(scalex, scale.y, scale.z);
+
+	}
+}
