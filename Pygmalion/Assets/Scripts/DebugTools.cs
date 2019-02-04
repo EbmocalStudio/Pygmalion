c53538792eabc6415cec4ba3dbbe0be492c94d92
Maxime Cloutier-Gravel <maxcg.private@gmail.com>
2019-02-03 21:00:30 -0500

    Text handling
    Currently the text manager is able to split a string into words and keep track of each word's and char's position and size in the world ( doesn't support "best fit" font size, RectTransform rotation or runtime resizing yet ).
    
    Todo :
    TextManager
     - Detect mouse when hovering a word.
     - Use a second Text object to highlight a specified word.
    
    Narration related
     - Create a way to timestamp words so that they can be highlighted in sync with the narration.

---
+++ Pygmalion/Assets/Scripts/DebugTools.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public static class DebugTools {
+	public static void DrawRectangle(Vector2 center, float halfWidth, float halfHeight, Color color, float duration) {
+		Debug.DrawLine(new Vector3(center.x - halfWidth, center.y + halfHeight, 0), new Vector3(center.x + halfWidth, center.y + halfHeight, 0), color, duration);
+		Debug.DrawLine(new Vector3(center.x + halfWidth, center.y + halfHeight, 0), new Vector3(center.x + halfWidth, center.y - halfHeight, 0), color, duration);
+		Debug.DrawLine(new Vector3(center.x + halfWidth, center.y - halfHeight, 0), new Vector3(center.x - halfWidth, center.y - halfHeight, 0), color, duration);
+		Debug.DrawLine(new Vector3(center.x - halfWidth, center.y - halfHeight, 0), new Vector3(center.x - halfWidth, center.y + halfHeight, 0), color, duration);
+	}
+}
